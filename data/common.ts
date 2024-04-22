a8dad67c7233a409cac15a027e336f0861167f68
Koraebi <63969373+koraebi@users.noreply.github.com>
2024-04-22 21:49:46 +0200

    Update resume

--- data/common.ts
+++ data/common.ts
@@ -1,6 +1,6 @@
 export const Pages: string[] = ['about', 'portfolio', 'contact'];
 export const Languages: string[] = ['en', 'fr'];
 export const ResumeLink: any = {
-  fr: "https://docs.google.com/document/d/e/2PACX-1vSY-iGW9kEaa1m5B2EEatCyytVOBejsmRGZFY7LQz8qM2iVCFgUNY0O9HxSuqlzLYH1W0yVZ0LBgodk/pub",
-  en: "https://docs.google.com/document/d/e/2PACX-1vSPZQ2b0GPDBNlhRFHxhNYJd5uq9uAIJe2XSqODGaNJF1dZARZxpJ8VIf9cqCJW5eJrHSMhUeMyAi7q/pub"
+  fr: "https://docs.google.com/document/d/e/2PACX-1vQ-cpPoMSBpXKBsIOQC0ADJHeB7jQuEooln5YVSBHTN7H-KdmmxbapLYsDkMhpmRSTAw9RaWulecHP7/pub",
+  en: "https://docs.google.com/document/d/e/2PACX-1vQ-cpPoMSBpXKBsIOQC0ADJHeB7jQuEooln5YVSBHTN7H-KdmmxbapLYsDkMhpmRSTAw9RaWulecHP7/pub"
 };
